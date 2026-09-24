8f63a8097f16159c1ec3bac66baf270628e05b28
adamEfraty <efraty665@gmail.com>
2026-09-24 12:25:11 +0300

    feat: redux

--- frontend/src/components/MessageList.tsx
+++ frontend/src/components/MessageList.tsx
@@ -1,13 +1,15 @@
-import {useState} from "react";
+import { useState } from "react";
 import Message from "./Message";
 
 function MessageList() {
-    const [messages, setMessages] = useState<any[]>([]);
-    return (
-        <div className="message-list">
-            {messages.map((message, idx) => (
-                <Message key={idx} message={message} />
-                ))}
-        </div>
-    )
+  const [messages, setMessages] = useState<any[]>([]);
+  return (
+    <div className="message-list">
+      {messages.map((message, idx) => (
+        <Message key={idx} message={message} />
+      ))}
+    </div>
+  );
 }
+
+export default MessageList;
